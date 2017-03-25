9e593d116c68c6a973ef17a29fe3badadd4cfaae
Jaynarol <jaynarol@gmail.com>
2017-03-25 14:19:02 +0700

    fix redux template

--- react/src/components/App/index.js
+++ react/src/components/App/index.js
@@ -1,11 +1,3 @@
-import { connect } from 'react-redux'
 import App from './App'
-import { increaseNumber } from '../../actions/example'
 
-const mapStateToProps = state => ({
-  number: state.example.number
-})
-
-const mapDispatchToProps = { increaseNumber }
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
